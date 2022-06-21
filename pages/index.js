586cc2e709fd8de050d6d8c3bb19f177e1a2b073
Carlos Boyzo <carlos.boor@gmail.com>
2022-06-21 01:51:59 -0500

    ajuste

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
-import Logo from 'logo.png';
+import Logo from '../public/logo.png';
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -10,12 +10,11 @@ export default function Home() {
         <meta name='description' content='Rally Apoyandote® Eisai' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-
       <main className={styles.main}>
-        <Image src={Logo} alt='Rally Apoyandote®' width={200} height={200} />
+        <Image src={Logo} alt='Rally Apoyandote®' width={296.54} height={78} />
         <h1 className={styles.title}>Bienvenid@ a Rally Apoyandote</h1>
       </main>
-
+      a
       <footer className={styles.footer}>
         <a
           href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
