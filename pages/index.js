45cbe2e61d8d4bdf25b3e578fcf0b7faabf64c02
Carlos Boyzo <carlos.boor@gmail.com>
2022-06-21 11:04:01 -0500

    ajustes og image

--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,29 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import Logo from '../public/logo.png';
+
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Rally Apoyandote®</title>
         <meta name='description' content='Rally Apoyandote® Eisai' />
-        <link rel='icon' href='/favicon.ico' />
+        <link rel='icon' href='/icon.png' />
+        <meta property="og:image" content="/portada.png" />
       </Head>
       <main className={styles.main}>
         <Image src={Logo} alt='Rally Apoyandote®' width={296.54} height={78} />
-       
       </main>
-      
+
       <footer className={styles.footer}>
-      <span className={styles.logo}>
-            <Image src='/logos.png' alt='Vercel Logo' width={344.75} height={46} />
-          </span>
+        <span className={styles.logo}>
+          <Image
+            src='/logos.png'
+            alt='Vercel Logo'
+            width={344.75}
+            height={46}
+          />
+        </span>
       </footer>
     </div>
   );
