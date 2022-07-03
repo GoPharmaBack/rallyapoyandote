7a1d98e940e7a254afb1666467e53626bc1f4bb8
Carlos Boyzo <carlos.boor@gmail.com>
2022-07-02 23:57:26 -0500

    ajuste

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,26 +1,33 @@
 import Image from 'next/image';
 import Logo from '../img/logo.png';
 import styles from '../styles/Navbar.module.scss';
-import Link from 'next/link'
+import Link from 'next/link';
 function Nav() {
   return (
     <>
       <nav className={styles.navbar}>
         <div className={styles.contenedorNav}>
-      
-            <Image
-              src={Logo}
-              alt='Rally Apoyandote®'
-              width={170}
-              height={50}
-              className={styles.logoNav}
-            />
-         
-          
+          <Image
+            src={Logo}
+            alt='Rally Apoyandote®'
+            width={170}
+            height={50}
+            className={styles.logoNav}
+          />
+
           <div className={styles.menu}>
             <ul>
               <Link href='/rallyapoyandote'>
-                <li className={styles.item}>RALLY APOYÁNDOTE<sup style={{translateY:"translateY(-2px)", position:"absolute"}}>®</sup>  </li>
+                <li className={styles.item}>
+                  RALLY APOYÁNDOTE
+                  <sup
+                    style={{
+                      translateY: 'translateY(-2px)',
+                      position: 'absolute',
+                    }}>
+                    ®
+                  </sup>{' '}
+                </li>
               </Link>
               <Link href='/pits'>
                 <li className={styles.item}>PITS</li>
--- pages/coaching.jsx
+++ pages/coaching.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
-
+import Image from 'next/image';
+import React from 'react';
+import Footer from '../components/Footer';
+import Nav from '../components/Nav';
+import styles from '../styles/coaching.module.scss';
+import CoachingA from '../img/coachingA.png';
+import logo from '../img/logo.png';
 function coaching() {
   return (
-    <div>coaching</div>
-  )
+    <>
+      <Nav />
+      <section className={styles.coaching}>
+        <div className={styles.coachingContenedor}>
+          <div className='texto'>
+            <h2>COACHING</h2>
+            <p>¡Has venido al lugar adecuado!</p>
+            <p>
+              Para <span>conocer a profundidad</span> la <span>estrategia</span>
+              que tenemos para <span>triunfar</span> en
+            </p>
+            <Image
+              src={logo}
+              alt='Rally Apoyandote®'
+              width={170}
+              height={50}
+              className={styles.logoNav}
+            />
+          </div>
+          <div className={styles.botoneCoaching}>
+            <div className={styles.card1}>
+              <Image
+                src={CoachingA}
+                alt='coaching1'
+                className={styles.coachingImagen}
+                width='267px'
+                height='331px'
+              />
+              <div className={styles.coachingTexto}>
+                <h3>
+                  <span>Carlos</span> Gavira
+                </h3>
+                <p>ASSOCIATE DIRECTOR OF MARKETING</p>
+              </div>
+            </div>
+            <div className={styles.card2}>
+              <Image
+                src={CoachingA}
+                alt='coaching1'
+                className={styles.coachingImagen}
+                width='267px'
+                height='331px'
+              />
+              <div className={styles.coachingTexto}>
+                <h3>Xochitl Gomez</h3>
+                <p>MEDICAL DIRECTOR</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
 }
 
-export default coaching
+export default coaching;
