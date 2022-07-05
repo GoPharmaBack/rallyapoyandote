e21f88dedc6da86673226b9493a50c0e965b0f73
Carlos Boyzo <carlos.boor@gmail.com>
2022-07-05 16:46:08 -0500

    ajustes de ancho

--- components/ProgressBar.jsx
+++ components/ProgressBar.jsx
@@ -1,17 +1,17 @@
-import Image from "next/image";
+import Image from 'next/image';
 import carro from '../img/rosycarro.png';
 import styles from '../styles/envivo.module.scss';
 const ProgressBar = (props) => {
   const { bgcolor, completed, ruta, name } = props;
 
   const containerStyles = {
-    height: 60,
+    height: 50,
     width: '100%',
-    backgroundColor: "#e0e0de",
+    backgroundColor: '#e0e0de',
     borderRadius: 5,
-    margin: 10,
+    margin: 3,
     boxSizing: 'border-box',
-  }
+  };
 
   const fillerStyles = {
     height: '100%',
@@ -19,16 +19,15 @@ const ProgressBar = (props) => {
     backgroundColor: bgcolor,
     borderRadius: 'inherit',
     textAlign: 'right',
-    boxShadow: "0px 5px 10px #4241415e",
+    boxShadow: '0px 3px 13px #4241415e',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
     position: 'relative',
-
-  }
+  };
 
   const labelStyles = {
-    padding:15,
+    padding: 15,
     color: 'white',
     fontWeight: 'bold',
     display: 'flex',
@@ -36,38 +35,31 @@ const ProgressBar = (props) => {
     justifyContent: 'space-between',
     width: '100%',
     height: '100%',
-    fontSize: '1.2rem',
-    boxSizing: 'border-box',
+    fontSize: '1.1rem',
+    boxSizing: 'content-box',
     position: 'absolute',
     minWidth: '200px',
     borderRadius: 'inherit',
     boxSizing: 'border-box',
     left: '0',
-
-
-  }
+  };
 
   return (
     <>
-    
       <div style={containerStyles}>
-        
         <div style={fillerStyles}>
-          
           <span style={labelStyles}>
-          {`${name}`}
+            {`${name}`}
             <Image
               src={ruta}
-              width={"66px"}
-              height={"14px"}
+              width={'66px'}
+              height={'14px'}
               className={styles.carro}
-              alt="Carro"
-              layout="intrinsic"
+              alt='Carro'
+              layout='intrinsic'
             />
           </span>
-
         </div>
-
       </div>
     </>
   );
